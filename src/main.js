9624dbb59b8371f900c9d66e6d1edbf2d5936689
Shahin Sorkh <sorkh.shahin@hotmail.com>
2018-10-31 11:43:51 +0330

    standardjs styling fixes

--- src/main.js
+++ src/main.js
@@ -1,40 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import VueHighlightJS from 'vue-highlight.js';
+import VueHighlightJS from 'vue-highlight.js'
 
 /*
  * Use Vue Highlight.js
  */
-Vue.use(VueHighlightJS);
-import 'highlight.js/styles/github.css';
-
+Vue.use(VueHighlightJS)
+import 'highlight.js/styles/github.css'
 
 /**
  * Define some global variables
  */
 Vue.use({
-    install(Vue, options) {
-        Vue.prototype.$prefix = 'vpd-'; //shorted to reduce the css size
-    }
-});
-
+  install (Vue, options) {
+    Vue.prototype.$prefix = 'vpd-' // shorted to reduce the css size
+  }
+})
 
 /**
  * Import datepicker and define component
  */
-import VuePersianDatetimePicker from './picker/VuePersianDatetimePicker.vue';
-Vue.component('date-picker', VuePersianDatetimePicker);
+import VuePersianDatetimePicker from './picker/VuePersianDatetimePicker.vue'
+Vue.component('date-picker', VuePersianDatetimePicker)
 
-
-import Card from './components/card.vue';
-Vue.component('card', Card);
+import Card from './components/card.vue'
+Vue.component('card', Card)
 
 Vue.config.ignoredElements = [
-    'date-picker'
-];
+  'date-picker'
+]
 
 new Vue({
   el: '#app',
   render: h => h(App)
-});
+})
--- src/picker/modules/utils.js
+++ src/picker/modules/utils.js
@@ -1,88 +1,84 @@
 
-import moment from 'moment-jalaali';
+import moment from 'moment-jalaali'
 
-moment.loadPersian({dialect: 'persian-modern'});
+moment.loadPersian({dialect: 'persian-modern'})
 
+function addWeek (weekArray, week) {
+  let emptyDays = 7 - week.length
 
+  for (let i = 0; i < emptyDays; ++i) {
+    week[weekArray.length ? 'push' : 'unshift'](null)
+  }
 
-function addWeek(weekArray, week) {
-    let emptyDays = 7 - week.length;
-
-    for (let i = 0; i < emptyDays; ++i) {
-        week[weekArray.length ? 'push' : 'unshift'](null);
-    }
-
-    weekArray.push(week);
+  weekArray.push(week)
 }
 
-function getWeekArray(d, firstDayOfWeek) {
+function getWeekArray (d, firstDayOfWeek) {
+  let daysInMonth = moment.jDaysInMonth(moment(d).jYear(), moment(d).jMonth())
 
-    let daysInMonth = moment.jDaysInMonth(moment(d).jYear(), moment(d).jMonth());
+  let dayArray = []
+  for (let i = 1; i <= daysInMonth; i++) {
+    dayArray.push(moment(d).jDate(i).toDate())
+  }
 
+  let weekArray = []
+  let week = []
 
-    let dayArray = [];
-    for (let i = 1; i <= daysInMonth; i++) {
-        dayArray.push(moment(d).jDate(i).toDate());
+  dayArray.forEach(function (day) {
+    if (week.length > 0 && day.getDay() === firstDayOfWeek) {
+      addWeek(weekArray, week)
+      week = []
     }
 
-    let weekArray = [];
-    let week = [];
+    week.push(day)
 
-    dayArray.forEach(function (day) {
-        if (week.length > 0 && day.getDay() === firstDayOfWeek) {
-            addWeek(weekArray, week);
-            week = [];
-        }
+    if (dayArray.indexOf(day) === dayArray.length - 1) {
+      addWeek(weekArray, week)
+    }
+  })
 
-        week.push(day);
-
-        if (dayArray.indexOf(day) === dayArray.length - 1) {
-            addWeek(weekArray, week);
-        }
-    });
-
-    return weekArray;
+  return weekArray
 }
 
-function getYearsList(from=1300, to=1450, range=false, date){
-    let years = [];
-    if(range){
-        let year = getYear(date);
-        from = year - range;
-        to = year + range;
-    }
-    for(let i = from; i <= to; i ++){
-        years.push(i);
-    }
-    return years;
+function getYearsList (from = 1300, to = 1450, range = false, date) {
+  let years = []
+  if (range) {
+    let year = getYear(date)
+    from = year - range
+    to = year + range
+  }
+  for (let i = from; i <= to; i++) {
+    years.push(i)
+  }
+  return years
 }
 
 // c = element to scroll to or top position in pixels
 // e = duration of the scroll in ms, time scrolling
 // d = (optative) ease function. Default easeOutCuaic
-function scrollTo(a,c,e,d){
-    d||(d=easeOutCuaic);a=a||document.documentElement;if(0===a.scrollTop){var b=a.scrollTop;++a.scrollTop;a=b+1===a.scrollTop--?a:document.body}b=a.scrollTop;0>=e||("object"===typeof b&&(b=b.offsetTop),"object"===typeof c&&(c=c.offsetTop),function(a,b,c,f,d,e,h){function g(){0>f||1<f||0>=d?a.scrollTop=c:(a.scrollTop=b-(b-c)*h(f),f+=d*e,setTimeout(g,e))}g()}(a,b,c,0,1/e,20,d))}
-function easeOutCuaic(t){t--;return t*t*t+1}
+function scrollTo (a, c, e, d) {
+  d || (d = easeOutCuaic); a = a || document.documentElement; if (a.scrollTop === 0) { var b = a.scrollTop; ++a.scrollTop; a = b + 1 === a.scrollTop-- ? a : document.body }b = a.scrollTop; e <= 0 || (typeof b === 'object' && (b = b.offsetTop), typeof c === 'object' && (c = c.offsetTop), (function (a, b, c, f, d, e, h) { function g () { f < 0 || f > 1 || d <= 0 ? a.scrollTop = c : (a.scrollTop = b - (b - c) * h(f), f += d * e, setTimeout(g, e)) }g() }(a, b, c, 0, 1 / e, 20, d)))
+}
+function easeOutCuaic (t) { t--; return t * t * t + 1 }
 
-function getMonthsList(minDate, maxDate, date){
-    let list = [],
-        min = minDate ? minDate.clone().startOf('jMonth').unix():-Infinity,
-        max = maxDate ? maxDate.clone().endOf('jMonth').unix():Infinity;
-    for(let i = 0; i < 12; i++){
-        let m = date.clone().jMonth(i);
-        if(m.clone().startOf('jMonth').unix() < min || m.clone().endOf('jMonth').unix() > max){
-            m.disabled = true;
-        }
-        list.push(m);
+function getMonthsList (minDate, maxDate, date) {
+  let list = [],
+    min = minDate ? minDate.clone().startOf('jMonth').unix() : -Infinity,
+    max = maxDate ? maxDate.clone().endOf('jMonth').unix() : Infinity
+  for (let i = 0; i < 12; i++) {
+    let m = date.clone().jMonth(i)
+    if (m.clone().startOf('jMonth').unix() < min || m.clone().endOf('jMonth').unix() > max) {
+      m.disabled = true
     }
-    return list;
+    list.push(m)
+  }
+  return list
 }
 
-
-export default  {
-    getWeekArray,
-    getYearsList,
-    getMonthsList,
-    scrollTo,
-    moment
-};
+export default {
+  getWeekArray,
+  getYearsList,
+  getMonthsList,
+  scrollTo,
+  moment
+}
