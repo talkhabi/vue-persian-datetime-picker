5896d20c6a84faecf1abe754b1d8a71f6f99d290
Mohamad Talkhabi <mt382861@gmail.com>
2019-08-19 10:13:37 +0430

    fix: autoprefixer config

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,12 @@ module.exports = {
             // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
-            scss: ['vue-style-loader', 'css-loader', 'sass-loader'],
+            scss: [
+              'vue-style-loader',
+              'css-loader',
+              'postcss-loader',
+              'sass-loader'
+            ],
             sass: [
               'vue-style-loader',
               'css-loader',
