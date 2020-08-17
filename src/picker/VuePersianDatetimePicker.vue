e23813f85439ac42f56eab89704ea39b6044a729
Morteza Kalantar <m.kalantar@dsi.ir>
2020-08-17 10:17:04 +0430

    refactor: merge convert and locale conditions

--- src/picker/VuePersianDatetimePicker.vue
+++ src/picker/VuePersianDatetimePicker.vue
@@ -1588,16 +1588,14 @@ export default {
       return date.clone()
     },
     convertToLocaleNumber(value) {
-      if (this.convertNumbers) {
-        if (this.locale == 'fa') {
-          return `${value}`.replace(/\d+/g, function(digit) {
-            var ret = ''
-            for (var i = 0, len = digit.length; i < len; i++) {
-              ret += String.fromCharCode(digit.charCodeAt(i) + 1728)
-            }
-            return ret
-          })
-        }
+      if (this.convertNumbers && this.locale == 'fa') {
+        return `${value}`.replace(/\d+/g, function(digit) {
+          var ret = ''
+          for (var i = 0, len = digit.length; i < len; i++) {
+            ret += String.fromCharCode(digit.charCodeAt(i) + 1728)
+          }
+          return ret
+        })
       }
       return value
     }
