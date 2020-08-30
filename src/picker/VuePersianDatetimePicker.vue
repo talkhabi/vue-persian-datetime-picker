47ffbe61fb87156e7801b7a2575a2aaa2d6b796b
Mohamad Talkhabi <mt382861@gmail.com>
2020-08-30 10:29:41 +0430

    style: lint

--- src/picker/VuePersianDatetimePicker.vue
+++ src/picker/VuePersianDatetimePicker.vue
@@ -1588,10 +1588,10 @@ export default {
       return date.clone()
     },
     convertToLocaleNumber(value) {
-      if (this.convertNumbers && this.locale == 'fa') {
+      if (this.convertNumbers && this.locale === 'fa') {
         return `${value}`.replace(/\d+/g, function(digit) {
-          var ret = ''
-          for (var i = 0, len = digit.length; i < len; i++) {
+          let ret = ''
+          for (let i = 0, len = digit.length; i < len; i++) {
             ret += String.fromCharCode(digit.charCodeAt(i) + 1728)
           }
           return ret
